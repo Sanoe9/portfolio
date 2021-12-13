6aba46b9b736fb84dce5144a9acb62b160299324
Salma Noe <sattari2101@gmail.com>
2021-12-13 14:58:01 -0800

    added the correct resume to the data folder

--- portfolio/src/data/introduction.js
+++ portfolio/src/data/introduction.js
@@ -1,7 +1,7 @@
 const intro = {
     introTitle: 'Hi there! I am Salma Noe.',
     introBody: 'I am a full-stack software engineer. I enjoy building full-stack applications. I also enjoy debugging and optimizing code.',
-    resume: 'https://docs.google.com/document/d/11zqRAltUciime050TOgnX1XWSqAi5lkQzR-KTwolyM8/edit#'
+    resume: 'https://drive.google.com/file/d/15ZjRYM4fmKQv5ctOrG2hE4IMU900XRrX/view?usp=sharing'
 };
 
 module.exports = intro;
