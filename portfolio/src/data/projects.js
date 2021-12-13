f7776114dfe2fc9a44f29424873d336869757004
Salma Noe <sattari2101@gmail.com>
2021-12-12 16:15:33 -0800

    commit

--- portfolio/src/data/projects.js
+++ portfolio/src/data/projects.js
@@ -1,16 +1,12 @@
-const projects = [ {name: 'Leaft',
+const projects = [ {name: 'Atelier',
+description: 'A full-stack application for e-commerce that allows the user to view product details, interact and purchase different products.',
+technologies: ['Javascript', 'React.js', 'React Material-UI', 'Node.js', 'CSS','Jest', 'Node.js', 'Express.js', 'MySQL', 'AWS', 'Redis', 'Nginx', 'Loader.io', 'K6'],
+github: 'https://github.com/Sanoe9/FEC-Atelier'}, {name: 'Leaft',
     description: 'A Lyft like full-stack application that allows users to host or find carpools.',
     technologies: ['Javascript', 'React.js', 'React Router', 'Jest', 'Node.js', 'Express.js', 'MySQL', 'AWS'],
-    gif: '',
-    github: 'https://github.com/BOC-LightSalmon/hr-rpp29-leaft'}, 
-    {name: 'Atelier',
-    description: 'A full-stack application for e-commerce that allows the user to view product details, interact and purchase different products.',
-    technologies: ['Javascript', 'React.js', 'React Material-UI', 'Node.js', 'CSS','Jest', 'Node.js', 'Express.js', 'MySQL', 'AWS', 'Redis', 'Nginx', 'Loader.io', 'K6'],
-    gif: '',
-    github: 'https://github.com/Sanoe9/FEC-Atelier'}, {name: 'WeeCubs',
+    github: 'https://github.com/BOC-LightSalmon/hr-rpp29-leaft'}, {name: 'WeeCubs',
     description: 'A full-stack application that keeps track of newborns\' feedings and other activities.',
     technologies: ['Javascript','React.js', 'Chart.js', 'Node.js', 'Express.js', 'MySQL', 'AWS', 'Redis', 'Nginx', 'Loader.io', 'K6'],
-    gif: '',
     github: 'https://github.com/Sanoe9/mvp'}
 ];
 
