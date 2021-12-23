29dad0867cc65185032ee35fcea1283140a795b1
Salma Noe <sattari2101@gmail.com>
2021-12-22 21:48:05 -0800

    commit

--- portfolio/src/data/projects.js
+++ portfolio/src/data/projects.js
@@ -3,7 +3,7 @@ description: 'A full-stack application for e-commerce that allows the user to vi
 technologies: ['Javascript', 'React.js', 'React Material-UI', 'Node.js', 'CSS','Jest', 'Node.js', 'Express.js', 'MySQL', 'AWS', 'Redis', 'Nginx', 'Loader.io', 'K6'],
 github: 'https://github.com/Sanoe9/FEC-Atelier'}, {name: 'Leaft',
     description: 'A Lyft like full-stack application that allows users to host or find carpools.',
-    technologies: ['Javascript', 'React.js', 'React Router', 'Jest', 'Node.js', 'Express.js', 'MySQL', 'AWS'],
+    technologies: ['Javascript', 'React.js', 'React Router', 'Jest', 'Node.js', 'Express.js', 'MySQL', 'AWS', 'Socket.io'],
     github: 'https://github.com/BOC-LightSalmon/hr-rpp29-leaft'}, {name: 'WeeCubs',
     description: 'A full-stack application that keeps track of newborns\' feedings and other activities.',
     technologies: ['Javascript','React.js', 'Chart.js', 'Node.js', 'Express.js', 'MySQL', 'AWS', 'Redis', 'Nginx', 'Loader.io', 'K6'],
